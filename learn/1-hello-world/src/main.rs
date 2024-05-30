3aae4300abdc6bc2619e6e36569fbbf985c1c7c6
Sush Shringarputale <sushring@microsoft.com>
2024-05-30 11:18:52 -0700

    Add lab code

---
+++ learn/1-hello-world/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    // THE COMPILER IS YOUR FRIEND!
+    let spam = vec!["cat", "dog", "mouse"];
+    println!("{}", spam[6]);
+
+    println!(42);
+}
---
+++ learn/2-ownership/src/main.rs
@@ -0,0 +1,41 @@
+use book::*;
+
+mod book;
+
+fn ownership() {
+
+    {
+        let s = "hello";
+
+        println!("{}", s);
+    }
+    println!("{}", s);
+
+}
+
+fn borrow() {
+    let i_robot = Book{};
+    loan(&i_robot, "foo"); // borrow
+    loan(&i_robot, "bar"); // borrow
+    
+    remove_book(i_robot); // give up ownership
+    
+    loan(&i_robot, "baz");
+}
+
+fn mutable_borrow() {
+    let mut i_robot = Book{};
+
+    edit(&mut i_robot, "foo"); // borrow mutably
+    edit(&mut i_robot, "bar");
+
+    sell(i_robot);
+    edit(&mut i_robot, "baz");
+}
+
+fn main() {
+    ownership();
+    borrow();
+    mutable_borrow();
+
+}
