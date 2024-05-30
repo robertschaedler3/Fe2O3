c94c9a3d413e97c10f1feb48c5d170f0fb233cf7
Sushrut Shringarputale <sushring@microsoft.com>
2024-05-30 11:20:52 -0700

    Add lab code (#9)

---
+++ learn/2-ownership/src/book.rs
@@ -0,0 +1,17 @@
+pub struct Book{}
+
+pub fn loan(_book: &Book, borrower: &str) {
+    println!("{} borrows the book", borrower);
+}
+
+pub fn remove_book(_book: Book) {
+    println!("Book is removed");
+}
+
+pub fn sell(_book: Book) {
+    println!("Book is sold");
+}
+
+pub fn edit(_book: &mut Book, editor: &str) {
+    println!("Book is edited by {}", editor);
+}
