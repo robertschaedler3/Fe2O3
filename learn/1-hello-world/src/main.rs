17e71092e07c6fe9164f9125e2c4ba4954667001
Robert Schaedler <rschaedler@microsoft.com>
2024-06-07 21:45:48 +0000

    clean up

--- learn/1-hello-world/src/main.rs
+++ learn/1-hello-world/src/main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    println!("Hello, world!");
-
-    // THE COMPILER IS YOUR FRIEND!
-    let spam = vec!["cat", "dog", "mouse"];
-    println!("{}", spam[6]);
-
-    println!(42);
+    let animals = ["cat", "dog", "mouse"];
+    println!("{}", animals[42]);
 }
--- learn/2-ownership/src/main.rs
+++ learn/2-ownership/src/main.rs
@@ -1,28 +1,31 @@
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+
 use book::*;
 
 mod book;
 
+#[cfg(feature="ownership")]
 fn ownership() {
-
     {
         let s = "hello";
-
         println!("{}", s);
     }
-    println!("{}", s);
 
+    println!("{}", s);
 }
 
+#[cfg(feature="borrow")]
 fn borrow() {
     let i_robot = Book{};
     loan(&i_robot, "foo"); // borrow
     loan(&i_robot, "bar"); // borrow
-    
+
     remove_book(i_robot); // give up ownership
-    
+
     loan(&i_robot, "baz");
 }
 
+#[cfg(feature="mu_borrow")]
 fn mutable_borrow() {
     let mut i_robot = Book{};
 
@@ -34,7 +37,12 @@ fn mutable_borrow() {
 }
 
 fn main() {
+    #[cfg(feature="ownership")]
     ownership();
+
+    #[cfg(feature="borrow")]
     borrow();
+
+    #[cfg(feature="mut_borrow")]
     mutable_borrow();
 }
