2f9e87503f6fa9256789e6d76e5d404421796a2b
Sush Shringarputale <sushring@microsoft.com>
2024-06-20 19:33:26 -0700

    Add tinker lab

--- tinker/src/crypto.rs
+++ tinker/src/crypto.rs
@@ -1,53 +1,37 @@
-use std::error::Error;
+use std::ffi::CStr;
 
-fn encdec(c: char, keychar: char, dec: bool) -> char {
-    if dec {
-        let mut cphr = c as i32 - 'a' as i32;
-        let keychar_i32 = keychar as i32 - 'a' as i32;
-        cphr = if cphr - keychar_i32 < 0 {
-            26 + (cphr - keychar_i32)
-        } else {
-            cphr - keychar_i32
-        };
-        cphr += 'a' as i32;
-        println!("Decipher char [{}], shift [{}], output [{}]", c as u8 as char, keychar_i32, cphr as u8 as char);
-        return cphr as u8 as char;
-    } else {
-        let c_i32 = c as i32;
-        let keychar_i32 = keychar as i32 - 'a' as i32;
-        let cphr = (c_i32 - 'a' as i32 + keychar_i32) % 26 + 'a' as i32;
-        println!("Cipher char {}, shift {}. output {}", c as u8 as char, keychar_i32, cphr as u8 as char);
-        return cphr as u8 as char;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub fn encrypt1_rs(text: String, key: i32) -> String {
+    unsafe {
+        let filecstr = std::ffi::CString::new(text).unwrap();
+        let out = encrypt1(filecstr.into_raw(), key);
+        CStr::from_ptr(out).to_str().unwrap().to_string()
     }
 }
 
-fn encryptdecrypt(str: String, key: String, dec: bool) -> Result<String, Box<dyn Error>> {
-    let mut index = 0;
-    let mut collector = Vec::new();
-    let keychars = key.as_bytes();
-    for i in str.as_str().chars() {
-        if i.is_alphabetic() {
-            let c = i.to_ascii_lowercase();
-            let keyindex = index % 5;
-            collector.push(
-                encdec(
-                    c,
-                    keychars[keyindex] as char,
-                    dec,
-                ),
-            );
-            index += 1;
-        } else if i.is_ascii() {
-            collector.push(i);
-        }
+pub fn decrypt1_rs(text: String, key: i32) -> String {
+    unsafe {
+        let filecstr = std::ffi::CString::new(text).unwrap();
+        let out = decrypt1(filecstr.into_raw(), key);
+        CStr::from_ptr(out).to_str().unwrap().to_string()
     }
-    return Ok(collector.into_iter().collect());
 }
 
-pub fn decrypt(encrypted_string: String, key: String) -> Result<String, Box<dyn Error>> {
-    return encryptdecrypt(encrypted_string, key.to_ascii_lowercase(), true);
+pub fn encrypt2_rs(text: String, key: String) -> String {
+    unsafe {
+        let filecstr = std::ffi::CString::new(text).unwrap();
+        let keycstr = std::ffi::CString::new(key).unwrap();
+        let out = encrypt2(filecstr.into_raw(), keycstr.into_raw());
+        CStr::from_ptr(out).to_str().unwrap().to_string()
+    }
 }
 
-pub fn encrypt(plain_string: String, key: String) -> Result<String, Box<dyn Error>> {
-    return encryptdecrypt(plain_string, key.to_ascii_lowercase(), false);
+pub fn decrypt2_rs(text: String, key: String) -> String {
+    unsafe {
+        let filecstr = std::ffi::CString::new(text).unwrap();
+        let keycstr = std::ffi::CString::new(key).unwrap();
+        let out = decrypt2(filecstr.into_raw(), keycstr.into_raw());
+        CStr::from_ptr(out).to_str().unwrap().to_string()
+    }
 }
--- tinker/src/main.rs
+++ tinker/src/main.rs
@@ -1,13 +1,11 @@
-mod crypto;
 mod tinker;
+mod crypto;
 use std::error::Error;
 
 use clap::Parser;
+use crypto::{decrypt1_rs, decrypt2_rs, encrypt1_rs, encrypt2_rs};
 
-use crate::{
-    crypto::{decrypt, encrypt},
-    tinker::crack,
-};
+use crate::tinker::*;
 
 fn parse_key(s: &str) -> Result<Option<String>, String> {
     if s.len() != 5 {
@@ -33,17 +31,32 @@ struct Args {
     #[arg(
         short,
         long,
-        conflicts_with = "encrypt",
+        conflicts_with = "encrypt1",
     )]
-    decrypt: bool,
+    decrypt1: bool,
 
     /// Encrypt mode
     #[arg(
         short,
         long,
-        conflicts_with = "decrypt",
+        conflicts_with = "decrypt1",
     )]
-    encrypt: bool,
+    encrypt1: bool,
+    /// Decrypt mode
+    #[arg(
+        short,
+        long,
+        conflicts_with = "encrypt2",
+    )]
+    decrypt2: bool,
+
+    /// Encrypt mode
+    #[arg(
+        short,
+        long,
+        conflicts_with = "decrypt2",
+    )]
+    encrypt2: bool,
 
     /// Encryption file
     #[arg(short, long, required = true, value_parser = parse_file)]
@@ -54,22 +67,35 @@ struct Args {
     outfile: Option<String>,
 
     #[arg(short, long, required = false, default_value = None, value_parser = parse_key)]
-    key: Option<String>,
+    key1: Option<i32>,
+    #[arg(short, long, required = false, default_value = None, value_parser = parse_key)]
+    key2: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Args::parse();
 
     // Open Encrypted File
-    if cli.decrypt {
+    if cli.decrypt1 {
         let encrypted_file = cli.file;
         println!("Decrypting...");
-        let out = decrypt(encrypted_file, cli.key.unwrap())?;
+        let out = decrypt1_rs(encrypted_file, cli.key1.unwrap());
         let outfile = cli.outfile.unwrap_or("out.txt".to_string());
         std::fs::write(outfile, out)?;
-    } else if cli.encrypt {
+    } else if cli.encrypt1 {
         println!("Encrypting...");
-        let out = encrypt(cli.file, cli.key.unwrap())?;
+        let out = encrypt1_rs(cli.file, cli.key1.unwrap());
+        let outfile = cli.outfile.unwrap_or("out.txt".to_string());
+        std::fs::write(outfile, out)?;
+    } else if cli.decrypt2 {
+        let encrypted_file = cli.file;
+        println!("Decrypting...");
+        let out = decrypt2_rs(encrypted_file, cli.key2.unwrap());
+        let outfile = cli.outfile.unwrap_or("out.txt".to_string());
+        std::fs::write(outfile, out)?;
+    } else if cli.encrypt2 {
+        println!("Encrypting...");
+        let out = encrypt2_rs(cli.file, cli.key2.unwrap());
         let outfile = cli.outfile.unwrap_or("out.txt".to_string());
         std::fs::write(outfile, out)?;
     } else {
@@ -78,4 +104,44 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Cracked key: {}", outkey)
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: Run crack functions and get the key
+    const key1: i32 = 10;
+    const key2: &str = "rust";
+    const key3: &str = "rustiscool";
+
+    #[test]
+    fn test_encrypted__1() {
+        let file = "input1/encrypted1.txt";
+        let result = decrypt1_rs(file.to_string(), key1.into());
+        
+        // Hash the result
+        let result = sha256::digest(result.as_bytes());
+        assert_eq!(result, "16b1a5e0e6db690416b4cc00e878ede9a2a61ef3ed3a848a4dd933fe199539b4");
+    }
+
+    #[test]
+    fn test_encrypted__2() {
+        let file = "input1/encrypted2.txt";
+        let result = decrypt2_rs(file.to_string(), key2.into());
+        
+        // Hash the result
+        let result = sha256::digest(result.as_bytes());
+        assert_eq!(result, "16b1a5e0e6db690416b4cc00e878ede9a2a61ef3ed3a848a4dd933fe199539b4");
+    }
+
+    #[test]
+    fn test_encrypted__3() {
+        let file = "input1/encrypted3.txt";
+        let result = decrypt2_rs(file.to_string(), key3.into());
+        
+        // Hash the result
+        let result = sha256::digest(result.as_bytes());
+        assert_eq!(result, "16b1a5e0e6db690416b4cc00e878ede9a2a61ef3ed3a848a4dd933fe199539b4");
+    }
 }
